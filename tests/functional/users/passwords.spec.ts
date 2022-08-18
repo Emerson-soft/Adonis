5b7ac67a02e0d783a1a4cdc995f5938d0776b74c
Zanng <emerson_10_s@hotmail.com>
2022-08-18 00:07:39 -0300

    configure Mail

--- tests/functional/users/passwords.spec.ts
+++ tests/functional/users/passwords.spec.ts
@@ -1,6 +1,7 @@
 import { UserFactory } from './../../../database/factories/index'
 import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
+import Mail from '@ioc:Adonis/Addons/Mail'
 
 test.group('password', (group) => {
   group.each.setup(async () => {
@@ -11,11 +12,14 @@ test.group('password', (group) => {
   test('it should send and email with forgot password instructions', async ({ client }) => {
     const { email } = await UserFactory.create()
 
+    const fakerMail = Mail.fake()
+    fakerMail.exists({ subject: 'Roleplay: Recuperação de senha' })
     const userResetPassword = await client.post('/forgot-password').json({
       email,
       resertPasswordUrl: 'url',
     })
 
+    Mail.restore()
     userResetPassword.assertStatus(204)
   })
 })
